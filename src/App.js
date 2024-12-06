140fcc9e7d615fd789cabbd6d477015139f5d060
Abdul Saboor <abdulsaboor.xwave@gmail.com>
2024-12-06 11:24:50 +0500

    kamam

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Header from './Components/Header/header';
+function App() {
+  return (
+    <div className="App">
+<Header/>
+
+
+<h1>My React</h1>
+    </div>
+  );
+}
+
+export default App;
